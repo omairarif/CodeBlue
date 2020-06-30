8c01aa7b57bb2ad26ef304c6f87ddd8da7fed8a4
omairarif <40460752+omairarif@users.noreply.github.com>
2020-06-30 23:21:47 +0500

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,25 @@
+
+(function ($) {
+    "use strict";
+
+
+    /*==================================================================
+    [ Focus Contact2 ]*/
+    $('.input2').each(function(){
+        $(this).on('blur', function(){
+            if($(this).val().trim() != "") {
+                $(this).addClass('has-val');
+            }
+            else {
+                $(this).removeClass('has-val');
+            }
+        })    
+    })
+            
+  
+    
+
+    
+    
+
+})(jQuery);
